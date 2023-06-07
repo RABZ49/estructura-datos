2705a81f5da11551788d3ec4607187bdd7939f9d
Roberto <rabustamante5@gmail.com>
2023-06-07 09:46:03 -0500

    actualisacion de datos s1b2

--- Semana1b2/src/semana1b2/Ejecutor.java
+++ Semana1b2/src/semana1b2/Ejecutor.java
@@ -12,6 +12,7 @@ public class Ejecutor {
 
     public static void main(String[] args) {
         int op;
+        
         Metodos myLista = new Metodos();
         do {
             op = myLista.menu();
@@ -21,6 +22,15 @@ public class Ejecutor {
                     break;
                 case 2:
                     myLista.recorrer();
+                    break;
+                case 3:
+                    myLista.insInicio();
+                    break;
+                case 4:
+                    myLista.insOrden();
+                    break;
+                case 5:
+                    myLista.eliminar();
                     break;
 
                 default:
--- Semana1b2/src/semana1b2/Metodos.java
+++ Semana1b2/src/semana1b2/Metodos.java
@@ -16,15 +16,18 @@ public class Metodos {
     Nodo tail = null;
     Scanner entrada;
 
-    public  Metodos (){
+    public Metodos() {
 
         this.entrada = new Scanner(System.in);
     }
 
     public int menu() {
-        System.out.println("insertar [1]");
-        System.out.println("recorrer [2]");
-        System.out.println("salir    [0]");
+        System.out.println("insertar  final  [1]");
+        System.out.println("recorrer         [2]");
+        System.out.println("insertar inicio  [3]");
+        System.out.println("ordenar          [4]");
+        System.out.println("eliminar         [5]");
+        System.out.println("salir            [0]");
         int x = entrada.nextInt();
         return x;
 
@@ -51,27 +54,96 @@ public class Metodos {
             actual.sig = nuevo;
         }
     }*/
-    public void insertar(){
-    System.out.println("Ingrese dato: ");
+    public void insertar() {
+        System.out.println("Ingrese dato: ");
         int num = entrada.nextInt();
         Nodo nuevo = new Nodo(num);
-        if (listaVacia()){
+        if (listaVacia()) {
             head = nuevo;
             tail = nuevo;
-        }else{
-        tail.sig = nuevo;
-        tail = tail.sig;
+        } else {
+            tail.sig = nuevo;
+            tail = tail.sig;
         }
-    
+
     }
 
     public void recorrer() {
         Nodo actual = head;
-        while (actual!=null) {            
-            System.out.println("-> "+actual.dato);
+        while (actual != null) {
+            System.out.print(actual.dato + "-> ");
             actual = actual.sig;
-            
+
         }
         System.out.println("null");
     }
+
+    public void insInicio() {
+        System.out.println("Ingrese dato: ");
+        int num = entrada.nextInt();
+        Nodo nuevo = new Nodo(num);
+        if (listaVacia()) {
+            head = nuevo;
+            tail = nuevo;
+        } else {
+            nuevo.sig = head;
+            head = nuevo;
+        }
+
+    }
+
+    public void insOrden() {
+        System.out.println("Ingrese dato: ");
+        int num = entrada.nextInt();
+        Nodo nuevo = new Nodo(num);
+        if (listaVacia()) {
+            head = nuevo;
+            tail = nuevo;
+        } else {
+            if (num < head.dato) {
+                nuevo.sig = head;
+                head = nuevo;
+            } else {
+                Nodo actual = head;
+                if (true) {
+                    while ((actual.sig != null) && (num > actual.sig.dato)) {
+                        actual = actual.sig;
+
+                    }
+                    nuevo.sig = actual.sig;
+                    actual.sig = nuevo;
+                    if (nuevo.sig == null) {
+                        tail = nuevo;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void eliminar() {
+        System.out.println("ingrese el dato:");
+        int num = entrada.nextInt();
+        if (num == head.dato) {
+            if (head.sig == null) {
+                tail = null;
+
+            }
+            head = head.sig;
+        }
+        Nodo actual = head;
+        while ((actual.sig != null) && (actual.sig.dato != num)) {
+            actual = actual.sig;
+
+        }if (actual.sig == tail) {
+            tail = actual;
+            
+        }if (actual.sig!=null) {
+            actual.sig=actual.sig.sig;
+        }else{
+        System.out.println("El elemento no existe");
+        }
+
+    }
+
 }
